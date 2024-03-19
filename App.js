d5da1b21d5e954b8560ae929319edc2370ddf967
juju <132360412+jumduarte@users.noreply.github.com>
2024-03-19 16:28:06 -0300

    Imagem Expo

--- App.js
+++ App.js
@@ -1,20 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View, Image } from 'react-native';
+import { styles } from './AppStyle';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Taylor Swift diva</Text>
+      <Image style={styles.imagens}
+          source={require('./assets/TS.jpg')}
+      />
       <StatusBar style="auto" />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
